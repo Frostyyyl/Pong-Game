98011c3b70a24ebd00eb01c5aa74bf21357c1a4a
Frostyl <lgrobelny66@gmail.com>
2024-05-11 23:20:08 +0200

    Updated ball's movement mechanics

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class BallMovement : MonoBehaviour
@@ -11,14 +12,31 @@ public class BallMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ball.velocity = Vector2.left * initialSpeed;
+        ball.velocity = Vector2.up * initialSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Math.Sqrt(Math.Pow(ball.velocity.x, 2) + Math.Pow(ball.velocity.y, 2)) > 12){
+        if (Math.Sqrt(Math.Pow(ball.velocity.x, 2) + Math.Pow(ball.velocity.y, 2)) > maxSpeed){
             ball.velocity = ball.velocity.normalized * maxSpeed;
+        }
+
+
+        // Assign a new velocity if x velocity has deminished
+        if (Math.Abs(ball.velocity.x) < 0.25){
+            float currentVelocity = ball.velocity.y;
+            Vector2 newVelocity = new(0, 0);
+
+            float velocityX = UnityEngine.Random.Range(2, currentVelocity);
+            if (currentVelocity > maxSpeed / 2 && velocityX > currentVelocity / 2){
+                velocityX = UnityEngine.Random.Range(-currentVelocity / 2, 2);
+            } else if (currentVelocity < maxSpeed / 2 && velocityX < currentVelocity / 2){
+                velocityX = UnityEngine.Random.Range(-currentVelocity, -currentVelocity / 2);
+            }
+
+            newVelocity.Set(velocityX, (float)Math.Sqrt(Math.Pow(currentVelocity, 2) - Math.Pow(velocityX, 2)));
+            ball.velocity = newVelocity;
         }
     }
 }
